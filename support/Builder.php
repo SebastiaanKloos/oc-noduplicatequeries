4b4896c91fe73fd243b5c26e10184d061b3beacf
Sebastiaan Kloos <sebastiaankloos@hotmail.com>
2017-02-04 09:38:12 +0100

    Initial commit

---
+++ support/Builder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Codecycler\NoQueryDuplicates\Support;
+
+use Cache;
+use \Log;
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
+class Builder extends QueryBuilder
+{
+    /**
+     * Run the query as a "select" statement against the connection.
+     *
+     * @return array
+     */
+    protected function runSelect()
+    {
+        return Cache::store('request')->remember($this->getCacheKey(), 1, function() {
+            return parent::runSelect();
+        });
+    }
+
+    /**
+     * Returns a Unique String that can identify this Query.
+     *
+     * @return string
+     */
+    protected function getCacheKey()
+    {
+        return json_encode([
+            $this->toSql() => $this->getBindings()
+        ]);
+    }
+}
---
+++ traits/CacheQueryBuilder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Codecycler\NoQueryDuplicates\Traits;
+
+use Codecycler\NoQueryDuplicates\Support\Builder;
+
+trait CacheQueryBuilder
+{
+    /**
+     * Get a new query builder instance for the connection.
+     *
+     * @return \Illuminate\Database\Query\Builder
+     */
+    protected function newBaseQueryBuilder()
+    {
+        $conn = $this->getConnection();
+
+        $grammar = $conn->getQueryGrammar();
+
+        return new Builder($conn, $grammar, $conn->getPostProcessor());
+    }
+}
